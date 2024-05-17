4a6e79710f0c9f6dfdf9a47461b3d7a31522d8ae
Lilyppu <lily@ppu.co.id>
2024-05-17 11:42:10 +0700

    first commit

---
+++ src/main/java/com/Receiveable/ReceiveableApplication.java
@@ -0,0 +1,13 @@
+package com.Receiveable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiveableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiveableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Receiveable/ReceiveableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Receiveable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiveableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
